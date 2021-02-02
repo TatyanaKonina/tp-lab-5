dea105f23b67ae79e026066de75bea1a98920960
TatyanaKonina <63141760+TatyanaKonina@users.noreply.github.com>
2021-02-02 18:42:29 +0300

    Update data.h

--- include/data.h
+++ include/data.h
@@ -1,10 +1,11 @@
 // Copyright 2020 Konina Tatiana
-
+#ifndef INCLUDE_DATA_H_
+#define INCLUDE_DATAR_H_
 #include <vector>
 #include <map>
 #include <string>
 
 std::vector<std::map<int, std::string>> read_st(std::string input);
 std::vector<std::string> read_spec(std::string input);
-
+#endif  // INCLUDE_DATA_H_
 
