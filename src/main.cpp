a4629ed1b072d4fa815460925f4ac7b845174320
TatyanaKonina <63141760+TatyanaKonina@users.noreply.github.com>
2021-02-02 19:35:26 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,11 @@
 #include "Group.h"
 
 int main() {
-  Deanery* test = new Deanery("specList.txt","students.txt");
+  Deanery* test = new Deanery("specList.txt", "students.txt");
   test->initHeads();
   test->fireStudents();
   Group* gr = test->findGroup("Ancient landuages(assembler)", 1);
-  std::cout<<gr->getGroupSize();
- 
+  std::cout < <gr->getGroupSize();
   return 0;
 }
 
